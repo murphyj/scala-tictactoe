2636d973e62198c0501ed7c454cdf66a7f4c8d91
james.murphy <jamurphy84@hotmail.com>
2013-06-11 22:05:35 +0100

    Added more scala code for board implementation of TicTacToe

--- src/com/rookiecodes/Board.scala
+++ src/com/rookiecodes/Board.scala
@@ -2,8 +2,39 @@ package com.rookiecodes
 
 class Board {
   // array to store the board state
-  val board =
-  // move (player, position)
-  // isBoardWon
+  var currentBoard = List(new Tile(Marker.EMPTY), new Tile(Marker.EMPTY), new Tile(Marker.EMPTY),
+                   new Tile(Marker.EMPTY), new Tile(Marker.EMPTY), new Tile(Marker.EMPTY),
+                   new Tile(Marker.EMPTY), new Tile(Marker.EMPTY), new Tile(Marker.EMPTY))
+
+  var winningMoves = { (0, 1, 2), (3, 4, 5), (6, 7, 8),
+                       (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                       (0, 4, 8), (2, 4, 6)
+                     }
+
   // checkMove
+
+  def hasWon(marker: Marker) {
+     for (move <- 0 until winningMoves.length) {
+       if (move._1 === marker &&
+           move._2 === marker &&
+           move._3 === marker) {
+         return true
+       }
+     }
+    return false
+  }
+
+  def move(position: Int, marker: Marker) {
+    currentBoard.get(position).setMarker(marker)
+  }
+
+  def initialiseBoard() {
+      currentBoard.foreach { tile =>
+          tile.setMarker(Marker.EMPTY)
+      }
+  }
 }
+
+newBoard = new Board()
+newBoard.move(0, Marker.CROSS)
+
--- src/com/rookiecodes/Tile.scala
+++ src/com/rookiecodes/Tile.scala
@@ -1,5 +1,10 @@
 package com.rookiecodes
 
-class Tile(val marker:Tile) {
+import com.rookiecodes.Marker._
 
+class Tile(var marker:Marker) {
+
+  def setMarker(_marker: Marker) {
+     marker = _marker
+  }
 }
